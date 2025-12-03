39ce134f7e421c13d86563660d6e70a383c7f36d
ongbianca <bianca.denise.ong@student.ateneo.edu>
2025-12-03 17:36:12 +0800

    Delete models directory

--- models/Loyalty.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const LoyaltySchema = new mongoose.Schema({
-  customerName: { type: String, required: true },
-  points: { type: Number, default: 0 }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Loyalty", LoyaltySchema);
--- models/Service.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ServiceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  units: { type: Array, default: [] } // matches your JSON structure
-}, { timestamps: true });
-
-module.exports = mongoose.model("Service", ServiceSchema);
